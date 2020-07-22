d9c7737a29f0f6d6427a1423fb1493cf768f2d49
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-21 19:36:30 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Estoy en el app y no lo puedo editar :,v
         </p>
         <a
           className="App-link"
