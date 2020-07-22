dfdb86d99773e96f13a867b8ff59d0ebecd8820a
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-22 13:49:55 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+let getButtonText = () => {
+  return "Click On Me!";
+};
+
 // Create a React component
 const App = () => {
+  // const buttonText = "Click Me"; esto era inicialmente
+  const buttonText = { text: "Click Me!", text2: "Click Me!Bitch!" };
+  // <button style={{backgroundColor: '#000', color: 'white'}}>  asi era en un inicio
+  const style = {backgroundColor: '#000', color: 'white'}
+
   return (
     //cuando se tiene un jsx con multiples lineas se debe poner entre parentesis, en caso de que sea de una sola linea, la etiqueta debe estar al mismo nivel del return para que logre identificarlo.
     <div>
@@ -11,8 +20,9 @@ const App = () => {
         Enter your name:
       </label>
       <input id="name" type="text"></input>
-      <button style={{ backgroundColor: "#000", color: "white" }}>
-        Submit
+      <button style={style}>
+        {buttonText.text}
+        {/* {buttonText} */}
       </button>
     </div>
   );
