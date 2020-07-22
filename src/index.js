cbfc435d6264ec59195c433aab74bde16a15bdda
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-21 19:58:10 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Import the React and REACT DOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Create a React component
+
+
+//Take the react component and show it on the screen
