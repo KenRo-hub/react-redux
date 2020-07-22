d9ced126d3b2c4765199f94a0f14e82cb6690411
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-22 13:01:31 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Import the React and REACT DOM libraries
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a React component
 const App = () => {
-    return <div>My first JSX</div>;
+  return (//cuando se tiene un jsx con multiples lineas se debe poner entre parentesis, en caso de que sea de una sola linea, la etiqueta debe estar al mismo nivel del return para que logre identificarlo.
+  <div>
+      <label class="label" for="name">Enter your name:</label>
+      <input id="name" type="text"></input>
+      <button style="background-color:#000; color:white;">Submit</button>
+  </div>
+  );
 };
 
 //Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
