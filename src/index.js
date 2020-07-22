418c0a3f6d9ccf8c549306441fefe301f43149d9
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-21 20:07:54 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Create a React component
-
+const App = () => {
+    return <div>My first JSX</div>;
+};
 
 //Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
